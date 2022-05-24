5bfaa8636fdb1a5c5fa5274dfae355a8528c7db1
Azim <samsalievazim@gmail.com>
2022-05-24 16:01:50 +0600

    this is new_classwork

---
+++ product/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название категории")
+    slug = models.SlugField(primary_key=True, max_length=150, verbose_name="Слаг!")
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название продукта")
+    desc = models.TextField(max_length=500, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='product',
+                                 verbose_name="Категории")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.title
